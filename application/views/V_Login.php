98bf0297926bda34c0558bfd7265a5cb2fd57d3f
hamid <hamidmsukardi@gmail.com>
2018-11-01 13:29:49 +0700

    Tambahan Login v 1.0

--- application/views/V_Login.php
+++ application/views/V_Login.php
@@ -10,18 +10,23 @@
     <meta name="author" content="">
     <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png">
-    <title>Ela - Bootstrap Admin Dashboard Template</title>
+    <title>Login</title>
     <!-- Bootstrap Core CSS -->
-    <link href="<?php echo base_url(); ?>asstes/css/lib/bootstrap/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/css/lib/bootstrap/bootstrap.min.css" rel="stylesheet">
     <!-- Custom CSS -->
-    <link href="<?php echo base_url(); ?>asstes/css/helper-login.css" rel="stylesheet">
-    <link href="<?php echo base_url(); ?>asstes/css/style-login.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/css/helper-login.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/css/style-login.css" rel="stylesheet">
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:** -->
     <!--[if lt IE 9]>
     <script src="https:**oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https:**oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
 <![endif]-->
+    <style>
+        body {
+            background-color: #420089;
+        }
+    </style>
 </head>
 
 <body class="fix-header fix-sidebar">
