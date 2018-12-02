131a5be0ecdc9b90c58fbf13fb909ee45f2a0260
hamid <hamidmsukardi@gmail.com>
2018-12-02 14:54:44 +0700

    Gallery Page Client v 1.1

--- application/views/V_Gallery.php
+++ application/views/V_Gallery.php
@@ -1,10 +1,15 @@
-<div class="gallery">
-	
-	<div class="header">
-		<h1>Testimoni Gambar</h1>
-	</div>
+<div id="colorlib-testimony" class="colorlib-light-grey">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6 col-md-offset-3 text-center colorlib-heading animate-box">
+						<span><i class="icon-star-full"></i><i class="icon-star-full"></i><i class="icon-star-full"></i></span>
+						<h2>Galeri</h2>
+						<p>We love to tell our successful far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+					</div>
+				</div>
+<div class="row">
+	<div class="col-md-12 animate-box">
 
-	<div class="row">
 		<div class="column">
 			<?php 
 
@@ -13,7 +18,7 @@
 			while ($data = mysqli_fetch_assoc($sql)) {
 			?>
 
-			<img src="assets/images/<?php echo $data['file_name']; ?>" alt="<?php echo $data['file_name']; ?>" style="width: 100%;">
+			<img class="animate-box gambar" src="assets/images/<?php echo $data['file_name']; ?>" alt="<?php echo $data['file_name']; ?>" style="width: 100%;">
 
 			<?php } ?>
 		</div>
@@ -21,15 +26,17 @@
 		<div class="column">
 			<?php 
 
-			$koneksi = mysqli_connect("localhost","root","","db_travel");
 			$sql = mysqli_query($koneksi, "SELECT * FROM tbl_gambar WHERE (id_gambar % 2) > 0");
 			while ($data = mysqli_fetch_assoc($sql)) {
 			?>
 
-			<img src="assets/images/<?php echo $data['file_name']; ?>" alt="<?php echo $data['file_name']; ?>" style="width: 100%;">
+			<img class="animate-box gambar" src="assets/images/<?php echo $data['file_name']; ?>" alt="<?php echo $data['file_name']; ?>" style="width: 100%;">
 
 			<?php } ?>
 		</div>
 	</div>
-
+</div>
+</div>
+</div>
+</div>
 </div>
