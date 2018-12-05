8149afee240df5579b298b02720f4f69dc19d1f8
hamid <hamidmsukardi@gmail.com>
2018-12-06 01:34:00 +0700

    Gallery Page v 2.0

--- application/views/MainMenu/V_Gallery.php
+++ application/views/MainMenu/V_Gallery.php
@@ -1,16 +1,17 @@
-
 <div class="masonry-gallery">
-    <div class="masonry-container">
+	<div class="masonry-container">
+    
 			<?php 
-
 			$conn = mysqli_connect("localhost","root","","db_travel");
 			$sql = mysqli_query($conn, "SELECT * FROM tbl_gambar");
 			while ($data = mysqli_fetch_assoc($sql)) {
 			?>
-
-				<img class="masonry-block animate-box" src="assets/images/<?php echo $data['file_name']; ?>" alt="<?php echo $data['file_name']; ?>">	
-
+			
+			<div class="masonry-block">
+				<img class="animate-box" src="assets/images/<?php echo $data['file_name']; ?>" alt="<?php echo $data['file_name']; ?>">	
+			</div>
+			
 			<?php } ?>
-		</div>
-	</div>
 
+	</div>
+</div>
